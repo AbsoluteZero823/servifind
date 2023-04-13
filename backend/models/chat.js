bfea5c877f09649c0adcccea96491da8987ff89a
PoorKit <zekiriel2000@gmail.com>
2023-04-13 12:56:28 +0800

    readable

--- backend/models/chat.js
+++ backend/models/chat.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const chatSchema = new mongoose.Schema({
   // chatName: { type: String, trim: true },
 
-  users: [{ type: mongoose.Schema.Types.ObjectId, ref: "user" }],
+  users: [{ 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "user" 
+  }],
   latestMessage: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Message",
