673e7de8f9a5e75e4ee7c68d18fa1d6390ae2cad
Kendrick Galan <absolutezero823@gmail.com>
2023-04-29 00:39:44 +0800

    forDeploy

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ const server = app.listen(process.env.PORT, () => {
 const io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
-        origin: ["http://localhost:3000", "https://servifind-api.onrender.com"],
+        origin: ["https://servifind-api.onrender.com"],
     },
 });
 
