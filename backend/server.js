ac9a408622bda8a9e5821fdc972fdd6878cc2372
Kendrick Galan <absolutezero823@gmail.com>
2023-04-28 23:30:36 +0800

    cors

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,16 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: 'backend/config/config.env' })
 
+const cors = require("cors");
+const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
+
+
 connectDatabase();
 
 cloudinary.config({
